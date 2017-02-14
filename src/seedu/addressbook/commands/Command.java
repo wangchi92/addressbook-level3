62345d8cae46e78181fca5f1a030e2ac86b1d391
wangchi <wangchi92@gmail.com>
2017-02-14 18:52:43 +0800

    - Add throws Exception to AddCommand::execute and Command::execute

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,7 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
 
     /**
      * Returns true for command types that mutate the data
